72760a681513191239052482575364aab48c9f80
mulhern <amulhern@redhat.com>
2015-10-26 09:34:35 -0400

    Add a test file for _types.py.
    Signed-off-by: mulhern <amulhern@redhat.com>

---
+++ tests/test_types.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2015  Red Hat, Inc.
+#
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions of
+# the GNU General Public License v.2, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY expressed or implied, including the implied warranties of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.  You should have received a copy of the
+# GNU General Public License along with this program; if not, write to the
+# Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.  Any Red Hat trademarks that are incorporated in the
+# source code or documentation are not subject to the GNU General Public
+# License and may only be used or replicated with the express permission of
+# Red Hat, Inc.
+#
+# Red Hat Author(s): Anne Mulhern <amulhern@redhat.com>
+
+"""
+    tests.test_types
+    ================
+
+    Tests types methods.
+
+    .. moduleauthor:: mulhern <amulhern@redhat.com>
+"""
+
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import pyblk
+
+
+class TestTypes(object):
+    """
+    Test functions over sets of types.
+    """
+    # pylint: disable=too-few-public-methods
+
+    def test_get_value(self):
+        """
+        Test getting an existing type, and not.
+        """
+        edge_type = pyblk.EdgeTypes.values()[0]
+        assert edge_type is pyblk.EdgeTypes.get_value(str(edge_type))
+
+        assert pyblk.EdgeTypes.get_value("bogus") is None
