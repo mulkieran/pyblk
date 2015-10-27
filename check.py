34eb2439e0e4b8ebfd2ceefc29965f7ee963f38e
mulhern <amulhern@redhat.com>
2015-10-27 15:57:55 -0400

    Extend check.py so that it handles ignored files.
    Signed-off-by: mulhern <amulhern@redhat.com>

--- check.py
+++ check.py
@@ -40,16 +40,36 @@ arg_map = {
    ]
 }
 
-def main():
+def get_parser():
+    """
+    Generate an appropriate parser.
+
+    :returns: an argument parser
+    :rtype: `ArgumentParser`
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument(
        "package",
        choices=arg_map.keys(),
        help="designates the package to test"
     )
-    args = parser.parse_args()
-    cmd = ["pylint", args.package] + arg_map[args.package]
-    return subprocess.call(cmd, stdout=sys.stdout)
+    parser.add_argument("--ignore", help="ignore these files")
+    return parser
+
+def get_command(namespace):
+    """
+    Get the pylint command for these arguments.
+
+    :param `Namespace` namespace: the namespace
+    """
+    cmd = ["pylint", namespace.package] + arg_map[namespace.package]
+    if namespace.ignore:
+        cmd.append("--ignore=%s" % namespace.ignore)
+    return cmd
+
+def main():
+    args = get_parser().parse_args()
+    return subprocess.call(get_command(args), stdout=sys.stdout)
 
 
 if __name__ == "__main__":
